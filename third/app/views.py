3a3029e128da40f58cec0fb2624fa72c614b6012
shirleychou1 <shirleychou1@gmail.com>
2015-06-21 21:46:12 +0800

    add hrr'

--- third/app/views.py
+++ third/app/views.py
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 from django.shortcuts import render_to_response
-# from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 # from django.template import RequestContext
 # from django import forms
 # from models import User
