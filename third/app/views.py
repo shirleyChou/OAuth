bec99cb757bfdfb9de90d6fcd4fcc9cfa8c322e4
shirleychou1 <shirleychou1@gmail.com>
2015-06-24 19:27:28 +0800

    .

--- third/app/views.py
+++ third/app/views.py
@@ -22,7 +22,7 @@ login_db_id = None
 
 def login(request):
     if login_db_id is not None:
-        return show_result
+        return show_result()
     else:
         return render_to_response('home.html')
 
@@ -134,7 +134,6 @@ def delete_account(request):
     return HttpResponseRedirect('/auth/logout/')
 
 
-@property
 def show_result():
     global login_db_id
 
