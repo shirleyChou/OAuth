2dce918ec08c387c7ab3e2bc2fe804a9bb21f178
shirleyChou <shirleyChou@users.noreply.github.com>
2015-06-29 15:13:49 +0800

    Update models.py

--- third/app/models.py
+++ third/app/models.py
@@ -4,3 +4,6 @@ class LoginInfo(models.Model):
     douban_id = models.CharField(max_length=50, blank=True)
     weibo_id = models.CharField(max_length=50, blank=True)
     qzone_id = models.CharField(max_length=50, blank=True)
+    douban_name = models.CharField(max_length=50, blank=True)
+    weibo_name = models.CharField(max_length=50, blank=True)
+    qzone_name = models.CharField(max_length=50, blank=True)
